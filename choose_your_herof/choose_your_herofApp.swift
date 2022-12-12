7607abdcdb4de2c60088dd1a49f7cd7df118c769
Sheludkov Ilya <sheludkov.ilya@gmail.com>
2022-12-12 13:36:15 +0600

    Initial Commit

---
+++ choose_your_herof/choose_your_herofApp.swift
@@ -0,0 +1,17 @@
+//
+//  choose_your_herofApp.swift
+//  choose_your_herof
+//
+//  Created by Шелудков Илья on 12.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct choose_your_herofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
